fe320683810af2bcae9c287104a2337f552c2cf0
jeremy-valcarcel <137318748+jeremy-valcarcel@users.noreply.github.com>
2023-07-18 23:50:00 -0300

    muestra

---
+++ Rompecabeza/TestRompecabezas.java
@@ -0,0 +1,20 @@
+public class TestRompecabezas{
+
+    public static void main(Strings[] args){
+
+        int[] myArray; //Declaración
+        myArray = new int[5]; // Inicialización
+        myArray[0] = 4; // agregar valor
+        myArray[1] = 8; // agregar valor
+        myArray[2] = 8; // agregar valor
+        myArray[3] = 5; // agregar valor
+        myArray[4] = 9; // agregar valor
+
+        String[] frutas = {"manzana", "mango", "sandia"};
+
+        System.out.println( myArray[0] +"My primer arreglo");
+        System.out.println(frutas +"arreglo frutas");
+        System.out.println(args + "ESTO ES ARGS");
+        
+    }
+}
